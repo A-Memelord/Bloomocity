18bad65de6b3a81aa620254b01dc8aa331f80cb6
MUKILTEO\1717482 <1717482@Mukilteo.Wednet.Edu>
2025-11-19 10:20:33 -0800

    pleb

--- Assets/Curtis/Scripts/Pickup.cs
+++ Assets/Curtis/Scripts/Pickup.cs
@@ -5,10 +5,13 @@ public class Pickup : MonoBehaviour
 {
     public Transform pickupPoint;
     private Quaternion rotation;
+    public Transform originalPoint;
+    private bool positionSet;
+    private bool pickedUp;
 
     void Start()
     {
-        
+        originalPoint = pickupPoint;
     }
 
     void Update()
@@ -18,20 +21,34 @@ public class Pickup : MonoBehaviour
             transform.localPosition = Vector3.zero;
             transform.localRotation = rotation;
         }
+
+        if (pickedUp)
+        {
+            pickupPoint.transform.Translate(Camera.main.transform.forward * Time.deltaTime * 300 * Input.GetAxis("Mouse ScrollWheel"), Space.World);
+        }
     }
 
     private void OnMouseDown()
     {
+        pickedUp = true;
+        if (!positionSet)
+        {
+            positionSet = true;
+            pickupPoint.position = transform.position;
+        }
         transform.parent = pickupPoint.transform;
         GetComponent<Rigidbody>().useGravity = false;
         GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         GetComponent<Rigidbody>().freezeRotation = true;
         GetComponent<BoxCollider>().enabled = false;
-        rotation = transform.rotation;
+        //rotation = transform.rotation;
     }
 
     private void OnMouseUp()
     {
+        pickedUp = false;
+        positionSet = false;
+        pickupPoint.position = originalPoint.position;
         transform.parent = null;
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<BoxCollider>().enabled = true;
