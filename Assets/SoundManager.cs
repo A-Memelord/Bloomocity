200ee185e88ce0bd9d149e273b0b7e5e029880fe
MUKILTEO\1722415 <1722415>
2025-11-25 08:10:56 -0800

    no message

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioSource soundEffectsSource;
+    public AudioSource musicSource;
+
     public AudioClip[] soundEffects;
 
     public AudioClip[] musicAudio;
 
     void Start()
     {
-        
+        PlaySoundMusic();
     }
 
     void Update()
     {
-        
+        if (!musicSource.isPlaying)
+        {
+            PlaySoundMusic();
+        }
+    }
+
+    public void PlaySoundMusic()
+    {
+        if (musicAudio.Length == 0) return;
+
+        AudioClip clip = musicAudio[Random.Range(0, musicAudio.Length)];
+        musicSource.clip = clip;
+        musicSource.Play();
     }
 }
