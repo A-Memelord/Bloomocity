ae70a1b1c9c4bc2d53b51ad68b153ab7b6982421
MUKILTEO\1717481 <1717481@mukilteo.wednet.edu>
2025-12-16 10:12:53 -0800

    tfgfhdhhsfdh

--- Assets/ed/plant_detail1.cs
+++ Assets/ed/plant_detail1.cs
@@ -7,7 +7,7 @@ public class plant_detail1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        print("plant detail created");
     }
 
     // Update is called once per frame
